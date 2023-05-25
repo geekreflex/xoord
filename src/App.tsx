d59b59cf616da8aa8c7ef19026961bb3dfdee302
jerry <jerrynwosu007@gmail.com>
2023-05-25 09:06:27 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import Canvas from '@/components/Canvas';
-import EditorComp from './components/core/EditorComp';
 
 export default function App() {
   return (
     <>
-      {/* <Canvas /> */}
-      <EditorComp />
+      <Canvas />
     </>
   );
 }
