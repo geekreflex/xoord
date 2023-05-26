9dbe1ec09dd518a966dcec438e513216e816b71d
jerry <jerrynwosu007@gmail.com>
2023-05-26 14:54:25 +0100

    feat: theme - prefers-color-scheme

--- src/context/ThemeContext.tsx
+++ src/context/ThemeContext.tsx
@@ -1,32 +1,57 @@
 import { Theme, darkTheme, lightTheme } from '@/themes/theme';
 import { ThemeProvider as StyledThemeProvider } from 'styled-components';
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 interface ThemeContextProps {
   theme: Theme;
+  mode: 'light' | 'dark';
   toggleTheme: () => void;
 }
 
 export const ThemeContext = createContext<ThemeContextProps>({
   theme: lightTheme,
+  mode: 'light',
   toggleTheme: () => {},
 });
 
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [currentTheme, setCurrentTheme] = useState(lightTheme);
+  const prefersDarkMode =
+    window.matchMedia &&
+    window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [currentTheme, setCurrentTheme] = useState(
+    prefersDarkMode ? darkTheme : lightTheme
+  );
+  const [currentMode, setCurrentMode] = useState<'light' | 'dark'>(
+    prefersDarkMode ? 'dark' : 'light'
+  );
 
   const toggleTheme = () => {
     setCurrentTheme((prevTheme) =>
       prevTheme === lightTheme ? darkTheme : lightTheme
     );
+    setCurrentMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
   };
+
+  useEffect(() => {
+    const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+    const handleDarkModeChange = (event: MediaQueryListEvent) => {
+      setCurrentTheme(event.matches ? darkTheme : lightTheme);
+      setCurrentMode(event.matches ? 'dark' : 'light');
+    };
+    mediaQueryList.addEventListener('change', handleDarkModeChange);
+    return () => {
+      mediaQueryList.removeEventListener('change', handleDarkModeChange);
+    };
+  }, []);
 
   const styledTheme: Theme = {
     ...currentTheme,
   };
 
   return (
-    <ThemeContext.Provider value={{ theme: currentTheme, toggleTheme }}>
+    <ThemeContext.Provider
+      value={{ theme: currentTheme, mode: currentMode, toggleTheme }}
+    >
       <StyledThemeProvider theme={styledTheme}>{children}</StyledThemeProvider>
     </ThemeContext.Provider>
   );
