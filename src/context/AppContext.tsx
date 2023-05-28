560a534d75b1fce1e7a91cd3ddf34d07d3805613
jerry <jerrynwosu007@gmail.com>
2023-05-28 12:28:34 +0100

    propery panel context setup

--- src/context/AppContext.tsx
+++ src/context/AppContext.tsx
@@ -2,27 +2,36 @@ import { ToolType } from '@/types/app';
 import { createContext, useContext, useState } from 'react';
 
 type AppContextType = {
-  toolPanel: string | null;
+  toolPanel: ToolType | null;
+  propertyPanel: string | null;
   openToolPanel: (tool: ToolType) => void;
   closeToolPanel: () => void;
 };
 
 const AppContext = createContext<AppContextType>({
   toolPanel: null,
+  propertyPanel: null,
   openToolPanel: () => {},
   closeToolPanel: () => {},
 });
 
 export const AppProvider = ({ children }: { children: React.ReactNode }) => {
   const [toolPanel, setToolPanel] = useState<ToolType | null>(null);
+  const [propertyPanel, setPropertyPanel] = useState<string | null>(null);
 
   const openToolPanel = (tool: ToolType) => setToolPanel(tool);
   const closeToolPanel = () => setToolPanel(null);
 
+  const openPropertyPanel = (property: string) => setPropertyPanel(property);
+  const closePropertyPanel = () => setPropertyPanel(null);
+
   const contextValue = {
     toolPanel,
+    propertyPanel,
     openToolPanel,
+    openPropertyPanel,
     closeToolPanel,
+    closePropertyPanel,
   };
 
   return (
