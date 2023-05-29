5a1cc4b0e2137785a803e9a089dd379548d5dcff
jerry <jerrynwosu007@gmail.com>
2023-05-30 00:44:18 +0100

    cleaned Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,61 +1,7 @@
-import { useEditorContext } from '@/context/EditorContext';
-import useStateWithHistory from '@/hooks/useStateWithHistory';
-import { useEffect } from 'react';
 import { styled } from 'styled-components';
-import { fabric } from 'fabric';
 import History from './toolbox/History';
 
 export default function Navbar() {
-  const { controller, editor } = useEditorContext();
-  const [canvas, setCanvas, { history, pointer, back, forward, go }] =
-    useStateWithHistory(editor?.canvas.toJSON());
-
-  useEffect(() => {
-    if (editor) {
-      // @ts-expect-error
-      editor.canvas.on({
-        'object:added': save,
-        'object:modified': save,
-        'selection:updated': save,
-      });
-    }
-  }, [editor]);
-
-  const save = (event: fabric.IEvent) => {
-    console.log(event.action);
-    setCanvas(editor?.canvas.toJSON());
-  };
-
-  const handleUndo = () => {
-    back();
-    updateCanvas();
-  };
-
-  const handleRedo = () => {
-    forward();
-    updateCanvas();
-  };
-
-  const updateCanvas = () => {
-    editor?.canvas.clear();
-    editor?.canvas.loadFromJSON(canvas, () => {
-      editor.canvas.renderAll();
-      setTimeout(() => {
-        console.log(editor.canvas);
-      }, 1000);
-    });
-  };
-
-  // const updateCanvas = () => {
-  //   delete canvas?.objects[0];
-  //   console.log(canvas);
-
-  //   editor?.canvas.loadFromJSON(canvas?.objects, () => {
-  //     editor.canvas.add(editor.workspace!);
-  //     editor.canvas.renderAll();
-  //   });
-  // };
-
   return (
     <NavbarWrap>
       <div>
