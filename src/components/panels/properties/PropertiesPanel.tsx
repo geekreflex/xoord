c4009004cae4f35d9f221ec2615097ba045ccff8
jerry <jerrynwosu007@gmail.com>
2023-05-30 20:23:42 +0100

    added selection types

--- src/components/panels/properties/PropertiesPanel.tsx
+++ src/components/panels/properties/PropertiesPanel.tsx
@@ -1,16 +1,24 @@
 import Panel from '@/components/common/Drawer';
 import { useEditorContext } from '@/context/EditorContext';
+import { useEffect } from 'react';
 
 export default function PropertiesPanel() {
-  const { editor, selectedObjects } = useEditorContext();
+  const { editor, selectedType, selectedObjects, clearSelectedObjects } =
+    useEditorContext();
 
   const onClosePropertyPanel = () => {
-    console.log(close);
+    clearSelectedObjects();
   };
+
+  useEffect(() => {
+    console.log(selectedObjects?.length);
+    console.log(selectedObjects);
+  }, [selectedObjects]);
 
   return (
     <Panel visible={!!selectedObjects} close={onClosePropertyPanel} pos="right">
       <div>Property panel</div>
+      <div>{selectedType}</div>
       {selectedObjects?.length}
     </Panel>
   );
--- src/context/EditorContext.tsx
+++ src/context/EditorContext.tsx
@@ -11,36 +11,38 @@ import { GridLine } from '@/core/guidlines';
 import { Controller } from '@/core/Controller';
 
 type EditorContextType = {
-  activeObject: fabric.Object | undefined;
   selectedObjects: fabric.Object[] | undefined;
+  selectedType: string | undefined;
   editor: Editor | null;
   gridLine: GridLine | null;
   elementTool: Element | null;
   controller: Controller | null;
   setEditor: (editor: Editor | null) => void;
+  clearSelectedObjects: () => void;
 };
 
 const EditorContext = createContext<EditorContextType>({
-  activeObject: undefined,
   selectedObjects: undefined,
+  selectedType: undefined,
   editor: null,
   gridLine: null,
   elementTool: null,
   controller: null,
   setEditor: () => {},
+  clearSelectedObjects: () => {},
 });
 
 export const EditorProvider = ({ children }: { children: React.ReactNode }) => {
   const [editor, _setEditor] = useState<Editor | null>(null);
   const [gridLine, setGridLine] = useState<GridLine | null>(null);
   const [elementTool, setElementTool] = useState<Element | null>(null);
-  const [activeObject, setActiveObject] = useState<fabric.Object | undefined>(
-    undefined
-  );
   const [controller, setController] = useState<Controller | null>(null);
   const [selectedObjects, setSelectedObjects] = useState<
     fabric.Object[] | undefined
   >(undefined);
+  const [selectedType, setSelectedType] = useState<string | undefined>(
+    undefined
+  );
 
   const onSetEditor = useCallback((editor: Editor | null) => {
     _setEditor(editor);
@@ -51,31 +53,57 @@ export const EditorProvider = ({ children }: { children: React.ReactNode }) => {
     }
   }, []);
 
+  const clearSelectedObjects = () => {
+    setSelectedObjects(undefined);
+    editor?.canvas.discardActiveObject();
+    editor?.canvas.renderAll();
+  };
+
   useEffect(() => {
     if (editor) {
-      editor.canvas.on('selection:created', (e) => {
+      const { canvas } = editor;
+
+      const handleSelection = (e: fabric.IEvent) => {
         const { selected } = e;
+
+        if (!selected) return;
+
+        if (selected?.length > 1) {
+          setSelectedType('Selection');
+        } else if (selected.length === 1) {
+          const firstSelectedObject = selected[0];
+          const objectType = firstSelectedObject.type;
+          setSelectedType(objectType);
+        } else {
+          setSelectedType('Unknown');
+        }
+
         setSelectedObjects(selected);
-        setActiveObject(
-          selected && selected.length > 0 ? selected[0] : undefined
-        );
+      };
+
+      canvas.on('selection:created', handleSelection);
+      canvas.on('selection:updated', handleSelection);
+      canvas.on('selection:cleared', () => {
+        setSelectedObjects(undefined);
       });
 
-      editor.canvas.on('selection:cleared', () => {
-        setSelectedObjects(undefined);
-        setActiveObject(undefined);
-      });
+      return () => {
+        canvas.off('selection:created', handleSelection);
+        canvas.off('selection:updated', handleSelection);
+        canvas.off('selection:cleared');
+      };
     }
   }, [editor]);
 
   const contextValues = {
-    activeObject,
     selectedObjects,
+    selectedType,
     editor,
     gridLine,
     elementTool,
     controller,
     setEditor: onSetEditor,
+    clearSelectedObjects,
   };
 
   return (
