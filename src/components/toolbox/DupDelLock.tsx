6e554d769a34e3773e29177471330f982e1b77a7
jerry <jerrynwosu007@gmail.com>
2023-05-30 11:43:05 +0100

    delete method

--- src/components/toolbox/DupDelLock.tsx
+++ src/components/toolbox/DupDelLock.tsx
@@ -1,10 +1,17 @@
 import { styled } from 'styled-components';
 import Icon from '../common/Icon';
+import { useEditorContext } from '@/context/EditorContext';
 
 export default function DupDelLock() {
+  const { controller } = useEditorContext();
+
+  const handleDelete = () => {
+    controller?.delete();
+  };
+
   return (
     <DupDelLockWrap>
-      <Icon name="trashIcon" size="medium" />
+      <Icon name="trashIcon" size="medium" click={handleDelete} />
       <Icon name="copyIcon" size="medium" />
       <Icon name="unlockIcon" size="medium" />
     </DupDelLockWrap>
