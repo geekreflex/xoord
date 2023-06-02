21cb21bb06d74cee2c5553b221f8ecd203135909
jerry <jerrynwosu007@gmail.com>
2023-06-02 12:39:26 +0100

    size and coord

--- src/components/panels/properties/widgets/SizeCoord.tsx
+++ src/components/panels/properties/widgets/SizeCoord.tsx
@@ -1,9 +1,80 @@
 import Icon from '@/components/common/Icon';
 import { Input } from '@/components/common/Input';
+import { useEditorContext } from '@/context/EditorContext';
 import { DividerX, Title } from '@/styles/global';
+import { useEffect, useState } from 'react';
 import { styled } from 'styled-components';
 
 export default function SizeCoord() {
+  const { selectedObjects, editor } = useEditorContext();
+  const [width, setWidth] = useState<string>('0');
+  const [height, setHeight] = useState<string>('0');
+  const [angle, setAngle] = useState<string>('0');
+  const [x, setX] = useState<string>('0');
+  const [y, setY] = useState<string>('0');
+
+  useEffect(() => {
+    const canvas = editor?.canvas;
+
+    if (canvas) {
+      canvas.on('object:scaling', handleSizing);
+      canvas.on('object:moving', handleMoving);
+      canvas.on('object:rotating', handleRotating);
+    }
+  }, [editor]);
+
+  const handleSizing = (e: fabric.IEvent) => {
+    const target = e.target as fabric.Object;
+    const newWidth = target.width! * target.scaleX!;
+    const newHeight = target.height! * target.scaleY!;
+    setWidth(newWidth.toFixed(2).toString());
+    setHeight(newHeight.toFixed(2).toString());
+  };
+
+  const handleMoving = (e: fabric.IEvent) => {
+    const target = e.target as fabric.Object;
+    const newX = target.left! * target.scaleX!;
+    const newY = target.top! * target.scaleY!;
+    setX(newX.toFixed(2).toString());
+    setY(newY.toFixed(2).toString());
+  };
+
+  const handleRotating = (e: fabric.IEvent) => {
+    const target = e.target as fabric.Object;
+    const newAngle = target.angle!;
+    setAngle(newAngle.toFixed(2).toString());
+  };
+
+  useEffect(() => {
+    if (selectedObjects) {
+      const x = selectedObjects[0].left!;
+      const y = selectedObjects[0].top!;
+
+      const w = selectedObjects[0].width!;
+      const h = selectedObjects[0].height!;
+
+      const a = selectedObjects[0].angle!;
+
+      setX(x.toFixed(2).toString());
+      setY(y.toFixed(2).toString());
+
+      setWidth(w.toFixed(2).toString());
+      setHeight(h.toFixed(2).toString());
+
+      setAngle(a.toFixed(2).toString());
+    }
+  }, [selectedObjects]);
+
+  // useEffect(() => {
+  //   if (selectedObjects) {
+  //     const obj = selectedObjects[0];
+  //     obj.set({
+  //       width: Number(width),
+  //       height: Number(height),
+  //     });
+  //   }
+  // }, [width, height, angle, x, y]);
+
   return (
     <SizeCoordWrap>
       <Title>Size & Coords</Title>
@@ -11,45 +82,23 @@ export default function SizeCoord() {
       <div className="main">
         <div className="size wrap-group">
           <Input
-            value={''}
-            onChange={function (e: any): void {
-              throw new Error('Function not implemented.');
-            }}
+            value={width}
+            onChange={(e) => setWidth(e.target.value)}
             sin={'W'}
           />
           <Input
-            value={''}
-            onChange={function (e: any): void {
-              throw new Error('Function not implemented.');
-            }}
+            value={height}
+            onChange={(e) => setHeight(e.target.height)}
             sin={'H'}
           />
-          <button className="lock btn-numb">
+          <div className="lock btn-numb">
             <Icon name="unlockIcon" hover={false} size="small" />
-          </button>
+          </div>
         </div>
         <div className="coord wrap-group">
-          <Input
-            value={''}
-            onChange={function (e: any): void {
-              throw new Error('Function not implemented.');
-            }}
-            sin={'X'}
-          />
-          <Input
-            value={''}
-            onChange={function (e: any): void {
-              throw new Error('Function not implemented.');
-            }}
-            sin={'Y'}
-          />
-          <Input
-            value={''}
-            onChange={function (e: any): void {
-              throw new Error('Function not implemented.');
-            }}
-            sin={'R'}
-          />
+          <Input value={x} onChange={() => {}} sin={'X'} />
+          <Input value={y} onChange={() => {}} sin={'Y'} />
+          <Input value={angle} onChange={() => {}} sin={'R'} />
         </div>
       </div>
     </SizeCoordWrap>
