88da567dbb111d693724cf0efc48767ac60f0fb9
jerry <jerrynwosu007@gmail.com>
2023-06-03 18:04:16 +0100

    working on custom number input

--- src/components/layouts/ChooseCanvasSize.tsx
+++ src/components/layouts/ChooseCanvasSize.tsx
@@ -1,9 +1,12 @@
 import Modal from '../common/Modal';
+import InputNum from '../common/input/InputNum';
 
 export default function ChooseCanvasSize() {
   return (
     <Modal title="Choose Template Size">
-      <div>----</div>
+      <div>
+        <InputNum />
+      </div>
     </Modal>
   );
 }
