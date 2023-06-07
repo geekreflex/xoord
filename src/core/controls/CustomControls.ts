569f59ddeb317eb9465d5b5994f18f3d9af61185
jerry <jerrynwosu007@gmail.com>
2023-06-07 20:31:25 +0100

    textbox ml scaling

--- src/core/controls/CustomControls.ts
+++ src/core/controls/CustomControls.ts
@@ -3,6 +3,7 @@ import verticalImg from '@/assets/editor/middlecontrol.svg';
 import horizontalImg from '@/assets/editor/middlecontrolhoz.svg';
 import edgeImg from '@/assets/editor/edgecontrol.svg';
 import rotateImg from '@/assets/editor/rotateicon.svg';
+import { scaleXOnly } from '../lib/scale';
 
 export class CustomControls {
   canvas: fabric.Canvas;
@@ -138,67 +139,10 @@ export class CustomControls {
       y: 0,
       offsetX: -1,
       cursorStyleHandler: fabric.controlsUtils.scaleSkewCursorStyleHandler,
-      actionHandler: fabric.controlsUtils.scalingXOrSkewingY,
+      actionHandler: scaleXOnly,
       getActionName: fabric.controlsUtils.scaleOrSkewActionName,
       render: renderVerticalIcon.bind(this),
     });
-
-    function getLocalPoint(
-      transform: fabric.Transform,
-      originX: any,
-      originY: any,
-      x: number,
-      y: number
-    ) {
-      var target = transform.target,
-        control = target.controls[transform.corner],
-        zoom = target.canvas!.getZoom(),
-        padding = target.padding! / zoom,
-        localPoint = target.toLocalPoint(
-          new fabric.Point(x, y),
-          originX,
-          originY
-        );
-      if (localPoint.x >= padding) {
-        localPoint.x -= padding;
-      }
-      if (localPoint.x <= -padding) {
-        localPoint.x += padding;
-      }
-      if (localPoint.y >= padding) {
-        localPoint.y -= padding;
-      }
-      if (localPoint.y <= padding) {
-        // current code
-        // if (localPoint.y <= -padding) { // expect code
-        localPoint.y += padding;
-      }
-      localPoint.x -= control.offsetX;
-      localPoint.y -= control.offsetY;
-      return localPoint;
-    }
-
-    const scaleXOnly = (
-      eventData: Event,
-      transform: fabric.Transform,
-      x: number,
-      y: number
-    ) => {
-      const target = transform.target as fabric.Object;
-      const scaleX = transform.scaleX || 1;
-      const localPoint = getLocalPoint(
-        transform,
-        transform.originX,
-        transform.originY,
-        x,
-        y
-      );
-      const strokePadding =
-        target.strokeWidth! / (target.strokeUniform ? scaleX : 1);
-      const newWidth = Math.abs(localPoint.x / scaleX) - strokePadding;
-      target.set('width', Math.max(newWidth, 0));
-      return true;
-    };
 
     fabric.Textbox.prototype.controls.mr = new fabric.Control({
       x: 0.5,
