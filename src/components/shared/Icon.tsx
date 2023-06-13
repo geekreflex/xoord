9edc966344093522b819d21867b900233fb46a4e
jerry <jerrynwosu007@gmail.com>
2023-06-13 11:41:25 +0100

    icons update

---
+++ src/components/shared/Icon.tsx
@@ -0,0 +1,92 @@
+import { IIcon } from '@/types/icon';
+import { styled } from 'styled-components';
+import { IconComponents } from '../icon/IconComponent';
+
+export default function Icon({
+  size = 'medium',
+  name,
+  color,
+  disabled,
+  hover = true,
+  title = null,
+  click,
+}: IIcon) {
+  const IconComponent = IconComponents[name];
+  const renderSize = () => {
+    switch (size) {
+      case 'small':
+        return '17px';
+      case 'medium':
+        return '20px';
+      case 'big':
+        return '24px';
+      default:
+        return '20px';
+    }
+  };
+
+  if (!IconComponent) {
+    console.warn(`Icon "${name}" not found.`);
+    return null;
+  }
+  return (
+    <IconWrap
+      size={renderSize()}
+      color={color}
+      disabled={disabled}
+      hover={hover.toString()}
+      t={title}
+      onClick={click}
+    >
+      <span id="icon">
+        <IconComponent />
+      </span>
+      {title && <span id="title">{title}</span>}
+    </IconWrap>
+  );
+}
+
+const IconWrap = styled.button<{
+  size?: string;
+  color?: string;
+  disabled?: boolean;
+  hover?: string;
+  t: string | null;
+}>`
+  display: flex;
+  font-size: ${(props) => props.size};
+  color: ${(props) => props.color || props.theme.colors.textColor};
+  opacity: ${(props) => (props.disabled ? 0.2 : 1)};
+  pointer-events: ${(props) => (props.disabled ? 'none' : 'auto')};
+  background-color: transparent;
+  outline: none;
+  border: none;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: ${(props) => (props.t ? '100%' : 'auto')};
+  height: ${(props) => (props.t ? '70px' : '')};
+  gap: 7px;
+  cursor: pointer;
+  padding: ${(props) => (props.hover == 'true' ? '5px' : '')};
+  border-radius: 5px;
+  &:hover {
+    background-color: ${(props) =>
+      props.hover == 'true' ? props.theme.colors.hoverColor1 : ''};
+  }
+
+  span#icon {
+    display: flex;
+  }
+
+  span#title {
+    font-size: 11px;
+    font-weight: 600;
+  }
+
+  svg,
+  path {
+    /* stroke-width: 2px; */
+  }
+`;
---
+++ src/types/icon.ts
@@ -0,0 +1,44 @@
+export interface IIcon {
+  size?: 'small' | 'medium' | 'big';
+  name: IconName;
+  color?: string;
+  disabled?: boolean;
+  hover?: boolean;
+  title?: string | null;
+  click?: () => void;
+}
+
+export type IconName =
+  | 'grid1Icon'
+  | 'zoomInIcon'
+  | 'zoomOutIcon'
+  | 'bgIcon'
+  | 'grid3Icon'
+  | 'textIcon'
+  | 'shapesIcon'
+  | 'imageIcon'
+  | 'closeIcon'
+  | 'close2Icon'
+  | 'lockIcon'
+  | 'unlockIcon'
+  | 'undoIcon'
+  | 'redoIcon'
+  | 'trashIcon'
+  | 'copyIcon'
+  | 'alignLeftIcon'
+  | 'alignBottomIcon'
+  | 'alignTopIcon'
+  | 'alignRightIcon'
+  | 'alignHorizontalIcon'
+  | 'alignVerticalIcon'
+  | 'brushIcon'
+  | 'brush2Icon'
+  | 'positionIcon'
+  | 'shadowIcon'
+  | 'layerIcon'
+  | 'arrowSwapXIcon'
+  | 'arrowSwapYIcon'
+  | 'arrowUpIcon'
+  | 'arrowDownIcon'
+  | 'settingsIcon'
+  | 'addImageIcon';
