3835163259fbdd1fba56d5698a423b063249cfa7
jerry <jerrynwosu007@gmail.com>
2023-06-16 10:35:14 +0100

    reset

--- src/components/tools/TextTool.tsx
+++ src/components/tools/TextTool.tsx
@@ -12,19 +12,5 @@ export default function TextTool() {
 
 const Wrap = styled.div`
   button {
-    width: 100%;
-    height: 40px;
-    border-radius: 4px;
-    border: 1px solid #fff;
-    background: transparent;
-    outline: none;
-    color: #fff;
-    cursor: pointer;
-    font-size: 14px;
-    font-weight: 600;
-    &:hover {
-      background-color: #fff;
-      color: #111;
-    }
   }
 `;
