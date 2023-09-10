9be8acfb4d1116c6c28c12edbd6a728f6e8fe9e5
jerry <jerrynwosu007@gmail.com>
2023-09-10 09:22:43 +0100

    feat: added color tool widget

--- src/components/Color.tsx
+++ src/components/Color.tsx
@@ -1,4 +1,13 @@
-import { Divider, Flex, Paper, createStyles } from '@mantine/core';
+import {
+  Divider,
+  Flex,
+  Paper,
+  createStyles,
+  ColorPicker,
+  Popover,
+  Box,
+  ColorInput,
+} from '@mantine/core';
 
 const useStyles = createStyles(() => ({
   block: {
@@ -39,12 +48,53 @@ export default function Color({ currentColor, colors, onColor }: IColor) {
         ))}
       </Flex>
       <Divider orientation="vertical" mx={10} />
-      <Paper
-        radius="lg"
-        className={classes.block}
-        bg={currentColor}
-        withBorder
-      />
+      <Popover offset={20} position="left-start" withArrow>
+        <Popover.Target>
+          <Paper
+            radius="lg"
+            className={classes.block}
+            bg={currentColor}
+            withBorder
+          />
+        </Popover.Target>
+        <Popover.Dropdown>
+          <ColorTool onColor={onColor} color={currentColor} />
+        </Popover.Dropdown>
+      </Popover>
     </Flex>
+  );
+}
+
+interface ColorToolProps {
+  onColor: (color: string) => void;
+  color: string;
+}
+
+function ColorTool({ onColor, color }: ColorToolProps) {
+  return (
+    <Box w={200}>
+      <ColorInput mb={10} value={color} onChange={onColor} />
+      <ColorPicker
+        format="hex"
+        value={color}
+        onChange={onColor}
+        swatches={[
+          '#25262b',
+          '#868e96',
+          '#fa5252',
+          '#e64980',
+          '#be4bdb',
+          '#7950f2',
+          '#4c6ef5',
+          '#228be6',
+          '#15aabf',
+          '#12b886',
+          '#40c057',
+          '#82c91e',
+          '#fab005',
+          '#fd7e14',
+        ]}
+      />
+    </Box>
   );
 }
